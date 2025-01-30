b9fcc25e795bd679957cfe92dba6a7e4cc68f09c
KufbsrieEwz <komododragon026@gmail.com>
2025-01-30 11:31:24 -0500

    Implemented simple drawing functions and the Vector2 class

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
+let canvas = document.getElementById('canvas')
+let c = canvas.getContext('2d')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+c.imageSmoothingEnabled = false
+class Vector2 {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
 
+    add(that) {
+        return new Vector2(this.x + that.x, this.y + that.y)
+    }
+
+    multiply(that) {
+        return new Vector2(this.x * that, this.y * that)
+    }
+}
+function drawRect(pos, dim, r, g, b, a) {
+    c.fillStyle = `rgba(${r}, ${g}, ${b}, ${a})`
+    c.fillRect(pos.x, pos.y, dim.x, dim.y)
+}
+
+function drawLine(list, r, g, b, a) {
+    c.strokeStyle = `rgba(${r}, ${g}, ${b}, ${a})`
+    c.beginPath()
+    c.moveTo(list[0].x, list[0].y)
+    for (let i of list) {
+        c.lineTo(i.x, i.y)
+    }
+    c.stroke()
+}
+
+function drawPoly(list, r, g, b, a) {
+    c.strokeStyle = `rgba(${r}, ${g}, ${b}, ${a})`
+    c.beginPath()
+    c.moveTo(list[0].x, list[0].y)
+    for (let i of list) {
+        c.lineTo(i.x, i.y)
+    }
+    c.stroke()
+    c.fill()
+}
+
+function drawArc(pos, rad, sa, ea, clock, r, g, b, a) {
+    c.strokeStyle = `rgba(${r}, ${g}, ${b}, ${a})`
+    c.fillStyle = `rgba(${r}, ${g}, ${b}, ${a})`
+    c.beginPath()
+    c.arc(pos.x, pos.y, rad, sa, ea, !clock)
+    c.stroke()
+    c.fill()
+}
+
+function drawImg(img, cropPos, cropDim, pos, dim) {
+    c.drawImage(img, cropPos.x, cropPos.y, cropDim.x, cropDim.y, pos.x, pos.y, dim.x, dim.y)
+}
+
+function write(text, pos, r, g, b, a) {
+    c.font = '20px Arial'
+    c.fillStyle = `rgba(${r}, ${g}, ${b}, ${a})`
+    c.fillText(text, pos.x, pos.y)
+}
+function clear() {
+    c.clearRect(0, 0, window.innerWidth, window.innerHeight)
+}
