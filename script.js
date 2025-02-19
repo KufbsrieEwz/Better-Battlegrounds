686726e65ff7e30b88fcf1a7a74f6305dab696bd
KufbsrieEwz <komododragon026@gmail.com>
2025-02-19 18:59:20 -0500

    Day 2
    Created tilemap, centered player, fixed bugs and began development on AI/enemies

--- script.js
+++ script.js
@@ -8,11 +8,9 @@ class Vector2 {
         this.x = x
         this.y = y
     }
-
     add(that) {
         return new Vector2(this.x + that.x, this.y + that.y)
     }
-
     multiply(that) {
         return new Vector2(this.x * that, this.y * that)
     }
@@ -84,23 +82,53 @@ class Attack {
     }
 }
 
-let player = new Player(new Vector2(0, 0), 100, 1, 'sword')
+class Enemy {
+    constructor(pos, health, damage, speed) {
+        this.pos = pos
+        this.health = health
+        this.damage = damage
+        this.speed = speed
+        this.target
+    }
+}
 
-let tilemap
+let player = new Player(new Vector2(6, 3), 100, 1, 'sword')
+
+let tilemap = []
+let tilemapRelativePos = {pos: new Vector2(0, 0)}
+for (let y = 0; y < 10; y++) {
+    tilemap[y] = []
+    for (let x = 0; x < 10; x++) {
+        tilemap[y][x] = (x + y) % 2
+    }
+}
 let attacks = []
+let all = [player, tilemapRelativePos].concat(attacks)
 let mouse = new Vector2(0, 0)
 document.addEventListener("keydown", function(event) {
     if (event.key == 'w') {
         player.pos.y -= 1
+        for (let i of all) {
+            i.pos.y += 1
+        }
     }
     if (event.key == 'a') {
         player.pos.x -= 1
+        for (let i of all) {
+            i.pos.x += 1
+        }
     }
     if (event.key == 's') {
         player.pos.y += 1
+        for (let i of all) {
+            i.pos.y -= 1
+        }
     }
     if (event.key == 'd') {
         player.pos.x += 1
+        for (let i of all) {
+            i.pos.x -= 1
+        }
     }
 })
 document.addEventListener("mousemove", function(event) {
@@ -129,10 +157,15 @@ document.addEventListener("click", function(event) {
 })
 function draw() {
     clear()
-    drawRect(player.pos.multiply(100), new Vector2(100, 100), 0, 0, 0, 1)
+    for (let y = 0; y < tilemap.length; y++) {
+        for (let x = 0; x < tilemap.length; x++) {
+            drawRect(new Vector2(x+tilemapRelativePos.pos.x, y+tilemapRelativePos.pos.y).multiply(100), new Vector2(1, 1).multiply(100), 0, (+!tilemap[y][x])*255, tilemap[y][x]*255, 1)
+        }
+    }
     for (let i of attacks) {
         drawRect(i.pos.multiply(100), new Vector2(50, 50), 255, 0, 0, 1)
     }
+    drawRect(player.pos.multiply(100), new Vector2(1, 1).multiply(100), 0, 0, 0, 1)
 }
 function run() {
     draw()
